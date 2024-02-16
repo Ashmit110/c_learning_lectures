829ca7e52ee426e3834e2e484730c80271a11827
Ashmit110 <89088826+Ashmit110@users.noreply.github.com>
2024-02-16 17:56:44 +0530

    palindrome check

--- loops.c
+++ loops.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
+#include<math.h>
+
+void palindrome(){
+    printf("enter a number to check for palindrome");
+    int n;
+    scanf("\n %d",&n);
+    int reversed=0;
+    int orig=n;
+    int ch=0;
+    while (n>0){
+        reversed=reversed*10+(n%10);
+        printf("%d \n",reversed);
+        
+        n=n/10;
+        if (reversed==orig)
+        {
+            printf("it is a palindrome number");
+        }
+        
+    }
+    printf("do you want to try another number ?");
+    scanf("%d \n",&ch);
+    return ch==1?palindrome():1;
+
+
+    
+}
+
  int main(int argc, char const *argv[])
  {
-    int input;
-    int count=0;
-    printf("%d",input);
-    while (input!=0)
-    {
-        printf("enter a non zero number");
-        scanf("%d", &input);
-        count++;
-    }
-    printf("you entered %d integers",count);
+    // int input;
+    // int count=0;
+    // printf("%d",input);
+    // while (input!=0)
+    // {
+    //     printf("enter a non zero number");
+    //     scanf("%d", &input);
+    //     count++;
+    // }
+    // printf("you entered %d integers",count);
+    // return 0;
+    // printf("%d",palindrome(12345));
+    palindrome();
     return 0;
  }
